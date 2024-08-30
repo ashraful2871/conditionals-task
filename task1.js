561c889db8adfac4c3ef880338eae9bee035b4ab
ashraful2871 <ashrafulislam2758@gmail.com>
2024-08-30 15:20:38 +0600

    task1 complete

---
+++ task1.js
@@ -0,0 +1,9 @@
+//free drinks
+
+const burgerPrice = 500;
+
+if (burgerPrice >= 500) {
+  console.log("Congratulations! You get A Free Mojo");
+} else {
+  console.log("you need buy a Mojo for 30tk");
+}
